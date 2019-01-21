a12dff561fd50ebb1e97e61dbbbd1f38ae7d3267
tru36rit <nathan.dunn.aml@gmail.com>
2019-01-20 20:02:50 -0600

    Makes utils

--- index.js
+++ index.js
@@ -1,77 +1,169 @@
-export const titleCase = str => {
-  return str
-    .split(" ")
-    .map(word =>
-      word
-        .split("")
-        .map((char, i) => (i === 0 ? char.toUpperCase() : char))
-        .join("")
-    )
-    .join(" ");
+// imports
+const indexOf = require("lodash/indexOf");
+const lastIndexOf = require("lodash/lastIndexOf");
+
+// helpers
+const plates = {
+  45: 10,
+  25: 1
 };
 
-export const longestWordInArray = arr => {
-  return arr.reduce((maxLen, word) => {
-    const len = word.word.length;
-    return len > maxLen ? len : maxLen;
-  }, 0);
+const roundTo = (number, round = 2.5, direction) => {
+  const dir =
+    direction === "up" ? "ceil" : (direction = "down" ? "floor" : "round");
+  return Number(Math[direction](number / round) * round);
 };
 
-export const roundTo = (number, round = 1, direction = "round") => {
-  if (direction) return Number(Math[direction](number / round) * round);
-};
+const powerSet = arr => {
+  let results = [null];
 
-export class Week {
-  constructor(week) {
-    this.week = week;
-    this.exercises = [];
+  for (let i = 0; i < arr.length; i++) {
+    let len = results.length;
+    for (let j = 0; j < len; j++) {
+      results.push(arr[i] + results[j]);
+    }
   }
 
-  addExercise(day, rx, bullet, percentage = []) {
-    if (day === "DAY") {
-      console.error(`You forgot to change the day on week ${this.week}`);
+  results.shift();
+  results = results.sort((a, b) => (a < b ? -1 : 1));
+  return results;
+};
+
+const makePossibleLoads = (plates, barWeight = 45) => {
+  const makePlatesArr = plates => {
+    const results = [];
+
+    for (let weight in plates) {
+      for (let i = 0; i < plates[weight]; i++) {
+        results.push(Number(weight));
+      }
+    }
+    return results;
+  };
+
+  const results = [barWeight];
+  const ps = powerSet(makePlatesArr(plates));
+
+  for (let p of ps) {
+    results.push(barWeight + 2 * p);
+  }
+
+  return Array.from(new Set(results));
+};
+
+const nudgeWeight = (load, precision, direction = "flat") => {
+  let result;
+  const originalWeight = load;
+
+  let platesToUse = { ...plates, 5: 2, 2.5: 2 }; // default
+  if (precision === 10) platesToUse = plates;
+  else if (precision === 5) platesToUse = { ...plates, 5: 2 };
+  else if (precision === 2.5) platesToUse = { ...plates, 5: 2, 2.5: 2 };
+  else if (precision === 1.25)
+    platesToUse = { ...plates, 5: 2, 2.5: 2, 1.25: 2 };
+  else console.log("PRECISION options are: 10, 5, 2,5, 1.25");
+
+  const loads = makePossibleLoads(platesToUse);
+
+  const minLoad = loads[0];
+  const maxLoad = loads[loads.length - 1];
+  let counter = 0;
+
+  //recursively move the wt up and down to find a matching easyload
+  const recurse = (load, s = true) => {
+    load = roundTo(load);
+    //base cases
+    if (loads.includes(load)) {
+      result = load;
+      return;
+    } else if (load < minLoad) {
+      result = roundTo(originalWeight, 2.5);
+      return;
+    } else if (load > maxLoad) {
+      result = roundTo(originalWeight, 5);
       return;
     }
-    const order = this.exercises.length;
 
-    let lift = "";
-    if (percentage.length) {
-      if (rx.toLowerCase().includes("bench")) lift = "bench";
-      else if (rx.toLowerCase().includes("press")) lift = "press";
-      else if (rx.toLowerCase().includes("squat")) lift = "squat";
-      else if (rx.toLowerCase().includes("deadlift")) lift = "deadlift";
-      else console.warning("NO LIFT!!!", rx);
+    //recursive cases
+    if (direction === "down") {
+      load--;
+      recurse(load);
+    } else if (direction === "up") {
+      load++;
+      recurse(load);
+    } else if (direction === "flat") {
+      if (s) {
+        counter++;
+        recurse(load + counter, !s);
+      } else if (!s) {
+        counter++;
+        recurse(load + counter * -1, !s);
+      }
     }
+  };
 
-    const exercise = {
-      week: String(this.week),
-      day,
-      order,
-      rx: String(rx),
-      bullet,
-      lift,
-      percentage,
-      instruction: null,
-      key: `${this.week}${day}${order}`
-    };
-    this.exercises[order] = exercise;
+  recurse(load);
+
+  return result;
+};
+
+const checkWorkWeight = workWeight => {
+  if (workWeight < 20 || isNaN(workWeight / 1)) {
+    throw new Error("Must provide valid workWeight > 20");
+  }
+};
+
+const combineLoadsAndReps = (loads, reps) => {
+  if (!reps || !reps.length) return loads;
+
+  const uniqueIndices = [];
+  for (let i = 0; i < loads.length; i++) {
+    const idx = indexOf(loads, loads[i]);
+    if (!uniqueIndices.includes(idx)) uniqueIndices.push(idx);
   }
 
-  addInstruction(message) {
-    this.exercises.forEach(
-      exercise =>
-        (exercise.instruction = {
-          week: this.week,
-          message
-        })
-    );
-  }
-}
+  const uniqueLoadsAndReps = uniqueIndices.map(uniqueIndex => {
+    return `${loads[uniqueIndex]}x${reps[uniqueIndex]}`;
+  });
 
-export const logWarmupPercentages = (lift, workWeight, warmups) => {
-  warmups
-    .split(", ")
-    .map(wu => ((wu.slice(0, wu.indexOf("x")) / workWeight) * 100).toFixed(1));
+  return uniqueLoadsAndReps;
+};
+
+const makeWarmupObj = (weight, workWeight, extra) => {
+  const percentage = (weight / workWeight).toFixed(2);
+  const reps =
+    percentage <= 0.7 ? 5 : percentage <= 0.8 ? 3 : percentage <= 0.9 ? 2 : 1;
+
+  const obj = {
+    weight,
+    reps,
+    percentage
+  };
+
+  return Object.assign({}, obj, extra && { [extra]: extra });
+};
+
+const endsInFive = n => n % 10 === 5;
+
+const nearestFive = weight => {
+  if (!weight) return weight;
+  weight = Math.round(weight);
+  if (endsInFive(weight)) return weight;
+
+  let upper = weight;
+  let lower = weight;
+
+  do {
+    upper++;
+    lower--;
+  } while (!endsInFive(upper) && !endsInFive(lower));
+
+  return endsInFive(upper) ? upper : lower;
+};
+
+// exports
+export const roundTo = (number, round = 1, direction = "round") => {
+  if (direction) return Number(Math[direction](number / round) * round);
 };
 
 export const calculateWeight = (reps, max) => {
@@ -97,25 +189,106 @@ export const calculateMax = (weight, reps) => {
   return roundTo(weight * reps * 0.033 + weight, 5, "floor");
 };
 
-export function boxMullerRandom() {
-  let phase = false,
-    x1,
-    x2,
-    w,
-    z;
+export const warmupIntensity = workWeight => {
+  checkWorkWeight(workWeight);
+  const percentages = [0.44, 0.55, 0.66, 0.77, 0.88];
+  const reps = [5, 4, 3, 2, 1];
 
-  return (function() {
-    if ((phase = !phase)) {
-      do {
-        x1 = 2.0 * Math.random() - 1.0;
-        x2 = 2.0 * Math.random() - 1.0;
-        w = x1 * x1 + x2 * x2;
-      } while (w >= 1.0);
+  const loads = percentages.map(percentage => workWeight * percentage);
+  const nudgedLoads = loads.map(load => nearestFive(load));
+  const warmups = nudgedLoads.map((weight, i) => {
+    return {
+      weight,
+      reps: reps[i],
+      percentage: Number((weight / workWeight).toPrecision(2))
+    };
+  });
+  return warmups;
+};
 
-      w = Math.sqrt((-2.0 * Math.log(w)) / w);
-      return x1 * w;
-    } else {
-      return x2 * w;
-    }
-  })();
-}
+export const warmupVolume = workWeight => {
+  checkWorkWeight(workWeight);
+  const reps = [4, 2];
+  const percentages = [0.6, 0.8];
+  const loads = percentages.map(percentage => {
+    return nudgeWeight(workWeight * percentage, percentage >= 0.9);
+  });
+  return combineLoadsAndReps(loads, reps);
+};
+
+export const warmupEasy = workWeight => {
+  checkWorkWeight(workWeight);
+  const reps = [5, 3, 2];
+  const percentages = [0.5, 0.75, 0.85];
+  const loads = percentages.map(percentage => {
+    return nudgeWeight(workWeight * percentage, percentage >= 0.9);
+  });
+  return combineLoadsAndReps(loads, reps);
+};
+
+export const warmupSteps = (workWeight, precision = 5) => {
+  const platesModerate = {
+    ...plates,
+    35: 1,
+    10: 1
+  };
+
+  let loads = makePossibleLoads(platesEasy, 135)
+    .filter(load => load <= workWeight * 0.95)
+    .map((weight, i) => {
+      const percentage = weight / workWeight;
+      return makeWarmupObj(weight, workWeight);
+    });
+
+  let lightLoads = makePossibleLoads(platesModerate, 45)
+    .filter(load => load <= workWeight * 0.95)
+    .map((weight, i) => {
+      const percentage = weight / workWeight;
+      return makeWarmupObj(weight, workWeight);
+    });
+
+  // ADDING TO THE BACK
+  const lastLoad = loads.length ? loads[loads.length - 1] : {};
+
+  if (lastLoad.percentage <= 0.75) {
+    const weight_1 = roundTo(workWeight * 0.85, 5, "round");
+    loads = [...loads, makeWarmupObj(weight_1, workWeight, "*")];
+  }
+
+  if (lastLoad.percentage <= 0.88) {
+    const weight = roundTo(workWeight * 0.95, 5, "down");
+    loads = [...loads, makeWarmupObj(weight, workWeight, "*")];
+  }
+
+  // ADDING TO THE FRONT
+  switch (loads.length) {
+    case 0:
+      return lightLoads;
+      return [
+        makeWarmupObj(roundTo(workWeight * 0.6, 5), workWeight, "*"),
+        makeWarmupObj(roundTo(workWeight * 0.7, 5), workWeight, "*"),
+        makeWarmupObj(roundTo(workWeight * 0.8, 5), workWeight, "*"),
+        makeWarmupObj(roundTo(workWeight * 0.9, 5), workWeight, "*")
+      ]; // TO DO
+    case 1:
+      return [
+        makeWarmupObj(65, workWeight, "*"),
+        makeWarmupObj(95, workWeight, "*"),
+        makeWarmupObj(115, workWeight, "*"),
+        ...loads
+      ];
+    case 2:
+      return [
+        makeWarmupObj(95, workWeight, "*"),
+        makeWarmupObj(115, workWeight, "*"),
+        ...loads
+      ];
+    case 3:
+      return [makeWarmupObj(95, workWeight, "*"), ...loads];
+    default:
+      return loads;
+  }
+};
+
+// testing
+// console.log(warmupIntensity(320));
